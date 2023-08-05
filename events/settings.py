719721aeed4269248b6f97e0c5b5d7cd73ca4b2e
Daniel <dterrero10@gmail.com>
2023-08-05 17:24:31 -0400

    update

--- events/settings.py
+++ events/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # settings.py
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
